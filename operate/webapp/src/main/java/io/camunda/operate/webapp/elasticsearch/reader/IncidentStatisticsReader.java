camunda/camunda

fix: Error message links on dashboard show wrong process instances

operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/IncidentStatisticsReader.java
@@ -104,7 +104,11 @@ public class IncidentStatisticsReader extends AbstractReader
             .field(IncidentTemplate.ERROR_MSG_HASH)
             .size(ElasticsearchUtil.TERMS_AGG_SIZE)
             .subAggregation(
-                topHits(ERROR_MESSAGE).size(1).fetchSource(IncidentTemplate.ERROR_MSG, null))
+                topHits(ERROR_MESSAGE)
+                    .size(1)
+                    .fetchSource(
+                        new String[] {IncidentTemplate.ERROR_MSG, IncidentTemplate.ERROR_MSG_HASH},
+                        null))
             .subAggregation(
                 terms(GROUP_BY_PROCESS_KEYS)
                     .field(IncidentTemplate.PROCESS_DEFINITION_KEY)
@@ -293,9 +297,11 @@ public class IncidentStatisticsReader extends AbstractReader
         ((TopHits) errorMessageBucket.getAggregations().get(ERROR_MESSAGE)).getHits();
     final SearchHit searchHit = searchHits.getHits()[0];
     final String errorMessage = (String) searchHit.getSourceAsMap().get(IncidentTemplate.ERROR_MSG);
+    final Integer errorMessageHash =
+        (Integer) searchHit.getSourceAsMap().get(IncidentTemplate.ERROR_MSG_HASH);
 
     final IncidentsByErrorMsgStatisticsDto processStatistics =
-        new IncidentsByErrorMsgStatisticsDto(errorMessage);
+        new IncidentsByErrorMsgStatisticsDto(errorMessage, errorMessageHash);
 
     final Terms processDefinitionKeyAggregation =
         (Terms) errorMessageBucket.getAggregations().get(GROUP_BY_PROCESS_KEYS);

operate/webapp/src/test/java/io/camunda/operate/rest/dto/incidents/IncidentsByErrorMsgStatisticsTest.java
@@ -16,10 +16,13 @@ public class IncidentsByErrorMsgStatisticsTest {
 
   @Test
   public void testCompareInstancesWithErrorCounts() {
-    final IncidentsByErrorMsgStatisticsDto first = new IncidentsByErrorMsgStatisticsDto("an error");
+    final String errorMessage = "an error";
+    final Integer errorMessageHash = errorMessage.hashCode();
+    final IncidentsByErrorMsgStatisticsDto first =
+        new IncidentsByErrorMsgStatisticsDto(errorMessage, errorMessageHash);
     first.setInstancesWithErrorCount(5);
     final IncidentsByErrorMsgStatisticsDto second =
-        new IncidentsByErrorMsgStatisticsDto("an error");
+        new IncidentsByErrorMsgStatisticsDto(errorMessage, errorMessageHash);
     first.setInstancesWithErrorCount(3);
     assertThat(IncidentsByErrorMsgStatisticsDto.COMPARATOR.compare(first, second)).isLessThan(0);
   }

camunda-cloud/zeebe

operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/IncidentStatisticsReader.java
@@ -104,7 +104,11 @@ public class IncidentStatisticsReader extends AbstractReader
             .field(IncidentTemplate.ERROR_MSG_HASH)
             .size(ElasticsearchUtil.TERMS_AGG_SIZE)
             .subAggregation(
-                topHits(ERROR_MESSAGE).size(1).fetchSource(IncidentTemplate.ERROR_MSG, null))
+                topHits(ERROR_MESSAGE)
+                    .size(1)
+                    .fetchSource(
+                        new String[] {IncidentTemplate.ERROR_MSG, IncidentTemplate.ERROR_MSG_HASH},
+                        null))
             .subAggregation(
                 terms(GROUP_BY_PROCESS_KEYS)
                     .field(IncidentTemplate.PROCESS_DEFINITION_KEY)
@@ -293,9 +297,11 @@ public class IncidentStatisticsReader extends AbstractReader
         ((TopHits) errorMessageBucket.getAggregations().get(ERROR_MESSAGE)).getHits();
     final SearchHit searchHit = searchHits.getHits()[0];
     final String errorMessage = (String) searchHit.getSourceAsMap().get(IncidentTemplate.ERROR_MSG);
+    final Integer errorMessageHashCode =
+        (Integer) searchHit.getSourceAsMap().get(IncidentTemplate.ERROR_MSG_HASH);
 
     final IncidentsByErrorMsgStatisticsDto processStatistics =
-        new IncidentsByErrorMsgStatisticsDto(errorMessage);
+        new IncidentsByErrorMsgStatisticsDto(errorMessage, errorMessageHashCode);
 
     final Terms processDefinitionKeyAggregation =
         (Terms) errorMessageBucket.getAggregations().get(GROUP_BY_PROCESS_KEYS);

operate/webapp/src/test/java/io/camunda/operate/rest/dto/incidents/IncidentsByErrorMsgStatisticsTest.java
@@ -16,10 +16,13 @@ public class IncidentsByErrorMsgStatisticsTest {
 
   @Test
   public void testCompareInstancesWithErrorCounts() {
-    final IncidentsByErrorMsgStatisticsDto first = new IncidentsByErrorMsgStatisticsDto("an error");
+    final String errorMessage = "an error";
+    final Integer errorMessageHashCode = errorMessage.hashCode();
+    final IncidentsByErrorMsgStatisticsDto first =
+        new IncidentsByErrorMsgStatisticsDto(errorMessage, errorMessageHashCode);
     first.setInstancesWithErrorCount(5);
     final IncidentsByErrorMsgStatisticsDto second =
-        new IncidentsByErrorMsgStatisticsDto("an error");
+        new IncidentsByErrorMsgStatisticsDto(errorMessage, errorMessageHashCode);
     first.setInstancesWithErrorCount(3);
     assertThat(IncidentsByErrorMsgStatisticsDto.COMPARATOR.compare(first, second)).isLessThan(0);
   }